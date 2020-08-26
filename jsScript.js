3469244e2674cba168b55ca525cb0c9314e966d2
JSBrenders <jean.sebastien.brenders@gmail.com>
2020-08-26 16:04:54 +0200

    v1.6

--- jsScript.js
+++ jsScript.js
@@ -1186,10 +1186,12 @@ function UpgradeBuildings() {
     var mblds = gamePage.bld.meta[0].meta.filter(res => res.stages && res.stages[1].stageUnlocked && res.stage == 0 && (res.name != "library" || gamePage.space.getProgram("orbitalLaunch").val == 1 ));
     var upgradeTarget;
     for (var up = 0; up < mblds.length; up++) {
-        upgradeTarget = gamePage.tabs[0].buttons.find(res => res.model.metadata && res.model.metadata.name == mblds[up].name);
-        // && res.model.metadata.name != "amphitheatre");
-        console.log(upgradeTarget)
-        upgradeByModel(upgradeTarget);
+        if (mblds[up].name != 'amphitheatre'){
+                upgradeTarget = gamePage.tabs[0].buttons.find(res => res.model.metadata && res.model.metadata.name == mblds[up].name);
+                // && res.model.metadata.name != "amphitheatre");
+                console.log(upgradeTarget)
+                upgradeByModel(upgradeTarget);
+        }
     }
 
     if (gamePage.bld.getBuildingExt('steamworks').meta.on < gamePage.bld.getBuildingExt('steamworks').meta.val && gamePage.resPool.get('coal').value > 0 && gamePage.bld.getBuildingExt('steamworks').meta.unlocked) {
